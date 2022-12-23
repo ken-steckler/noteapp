e61c8d3745b05b6591fca6dc2c14872fe228a64f
Ken Steckler <steckler92ken@gmail.com>
2022-12-23 11:17:17 -0800

    update ui

--- components/Nav.js
+++ components/Nav.js
@@ -1,33 +1,43 @@
-import Link from "next/link"
-import { auth } from "../utils/firebase"
-import { useAuthState } from "react-firebase-hooks/auth"
+import Link from "next/link";
+import { auth } from "../utils/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
-export default function Nav(){
-    const [user, loading] = useAuthState(auth);
-    return (
-        <nav className="flex justify-between items-center py-10">
-            <Link href="/">
-                <button className="text-lg font-medium">Creative Minds</button>
+export default function Nav() {
+  const [user, loading] = useAuthState(auth);
+  return (
+    <nav className="flex justify-between items-center py-10">
+      <Link href="/">
+        <button className="text-3xl font-medium bg-gradient-to-r from-blue-600 via-purple-500 to-indigo-500 inline-block text-transparent bg-clip-text">
+          Noteery
+        </button>
+      </Link>
+      <ul className="flex items-center gap-10">
+        {!user && (
+          <Link
+            href={"/auth/login"}
+            className="py-2 px-4 text-sm bg-gray-800 text-white rounded-lg font-medium ml-8"
+          >
+            Click to Join
+          </Link>
+        )}
+        {user && (
+          <div className="flex items-center gap-6">
+            <Link href="/post">
+              <button className="font-medium bg-blue-800 text-white py-2 px-4 rounded-mg text-sm">
+                Post
+              </button>
             </Link>
-            <ul className="flex items-center gap-10">
-                {!user && (
-                <Link href={'/auth/login'} className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">
-                    Join Now
-                </Link>
-                )}
-                {user && (
-                    <div className="flex items-center gap-6">
-                        <Link href="/post">
-                            <button className="font-medium bg-cyan-500 text-white py-2 px-4 rounded-mg text-sm">Post</button>
-                        </Link>
-                        <Link href="/dashboard">
-                            {/* https://stackoverflow.com/questions/40570117/http403-forbidden-error-when-trying-to-load-img-src-with-google-profile-pic */}
-                            <img className="w-12 rounded-full cursor-pointer" src={user.photoURL} referrerPolicy="no-referrer"/>
-                        </Link>
-                    </div>
-                )}
-            </ul>
-        </nav>
-    )
+            <Link href="/dashboard">
+              {/* https://stackoverflow.com/questions/40570117/http403-forbidden-error-when-trying-to-load-img-src-with-google-profile-pic */}
+              <img
+                className="w-12 rounded-full cursor-pointer"
+                src={user.photoURL}
+                referrerPolicy="no-referrer"
+              />
+            </Link>
+          </div>
+        )}
+      </ul>
+    </nav>
+  );
 }
-
--- components/message.js
+++ components/message.js
@@ -1,19 +1,20 @@
 export default function Message({ children, avatar, username, description }) {
-    return(
-        <div className="bg-white p-8 border-b-2 rounded-lg">
-            <div className="flex items-center gap-2">
-                {/* referrerPolicy: https://stackoverflow.com/questions/40570117/http403-forbidden-error-when-trying-to-load-img-src-with-google-profile-pic */}
-                <img src={avatar} className="w-10 rounded-full" referrerPolicy="no-referrer"/>
-                <h2>{username}</h2>
-            </div>
-            <div className="py-4">
-                <p>
-                    {description}
-                </p>
-            </div>
-            {/* see the posts */}
-            {children}
-        </div>
-        
-    )
+  return (
+    <div className="bg-white p-8 border-b-2 rounded-lg text-black m-10">
+      <div className="flex items-center gap-2">
+        {/* referrerPolicy: https://stackoverflow.com/questions/40570117/http403-forbidden-error-when-trying-to-load-img-src-with-google-profile-pic */}
+        <img
+          src={avatar}
+          className="w-10 rounded-full"
+          referrerPolicy="no-referrer"
+        />
+        <h2>{username}</h2>
+      </div>
+      <div className="py-4">
+        <p>{description}</p>
+      </div>
+      {/* see the posts */}
+      {children}
+    </div>
+  );
 }
